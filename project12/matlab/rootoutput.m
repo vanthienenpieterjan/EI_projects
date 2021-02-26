8e77d068a1a111685599e28e9888b8e55a43858b
pieterjanvanthienen <vanthienen.pieterjan@gmail.com>
2021-02-26 10:19:33 +0100

    error fix

--- project12/matlab/rootoutput.m
+++ project12/matlab/rootoutput.m
@@ -70,73 +70,73 @@ fileID = fopen(['../output/virtualVolumeOutPosZ1.dat']);
 virtualVolumeOutPosZ1 = fread(fileID,'float');
 fclose(fileID);
 
-% energy1
-fileID = fopen(['../output/energy1.dat']);
-energy1 = fread(fileID,'float');
+% energy1ff
+fileID = fopen(['../output/ffenergy1.dat']);
+energy1ff = fread(fileID,'float');
 fclose(fileID);
 
 % globalPosX1ff
-fileID = fopen(['../output/globalPosX1ff.dat']);
+fileID = fopen(['../output/ffglobalPosX1.dat']);
 globalPosX1ff = fread(fileID,'float');
 fclose(fileID);
 
 % globalPosY1ff
-fileID = fopen(['../output/globalPosY1.dat']);
+fileID = fopen(['../output/ffglobalPosY1.dat']);
 globalPosY1 = fread(fileID,'float');
 fclose(fileID);
 
 % globalPosZ1ff
-fileID = fopen(['../output/globalPosZ1ff.dat']);
+fileID = fopen(['../output/ffglobalPosZ1.dat']);
 globalPosZ1ff = fread(fileID,'float');
 fclose(fileID);
 
 % numberOfEntriesff
-fileID = fopen(['../output/numberOfEntriesff.dat']);
+fileID = fopen(['../output/ffnumberOfEntries.dat']);
 numberOfEntriesff = fread(fileID,'float');
 fclose(fileID);
 
 % perfectCrystalPosX1ff
-fileID = fopen(['../output/perfectCrystalPosX1ff.dat']);
+fileID = fopen(['../output/ffperfectCrystalPosX1.dat']);
 perfectCrystalPosX1ff = fread(fileID,'float');
 fclose(fileID);
 
 % perfectCrystalPosY1ff
-fileID = fopen(['../output/perfectCrystalPosY1ff.dat']);
+fileID = fopen(['../output/ffperfectCrystalPosY1.dat']);
 perfectCrystalPosY1ff = fread(fileID,'float');
 fclose(fileID);
 
 % perfectCrystalPosZ1ff
-fileID = fopen(['../output/perfectCrystalPosZ1ff.dat']);
+fileID = fopen(['../output/ffperfectCrystalPosZ1.dat']);
 perfectCrystalPosZ1ff = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeInPosX1ff
-fileID = fopen(['../output/virtualVolumeInPosX1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeInPosX1.dat']);
 virtualVolumeInPosX1 = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeInPosY1ff
-fileID = fopen(['../output/virtualVolumeInPosY1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeInPosY1.dat']);
 virtualVolumeInPosY1ff = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeInPosZ1ff
-fileID = fopen(['../output/virtualVolumeInPosZ1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeInPosZ1.dat']);
 virtualVolumeInPosZ1ff = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeOutPosX1ff
-fileID = fopen(['../output/virtualVolumeOutPosX1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeOutPosX1.dat']);
 virtualVolumeOutPosX1ff = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeOutPosY1ff
-fileID = fopen(['../output/virtualVolumeOutPosY1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeOutPosY1.dat']);
 virtualVolumeOutPosY1ff = fread(fileID,'float');
 fclose(fileID);
 
 % virtualVolumeOutPosZ1ff
-fileID = fopen(['../output/virtualVolumeOutPosZ1ff.dat']);
+fileID = fopen(['../output/ffvirtualVolumeOutPosZ1.dat']);
 virtualVolumeOutPosZ1ff = fread(fileID,'float');
 fclose(fileID);
 
